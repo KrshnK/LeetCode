d82c32ae109c1bf398b723626662d3906719d5d1
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-06-11 16:34:52 +0530

    Time: 44 ms (91.53%), Space: 48 MB (96.51%) - LeetHub

--- 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -14,19 +14,5 @@ function expect(val) {
         throw new Error("Equal");
       }
     }
-  };
+  }
 }
-
-// Test cases
-const func1 = () => ({ "value": expect(5).toBe(5) });
-console.log(func1()); // Output: {"value": true}
-
-try {
-  const func2 = () => expect(5).toBe(null);
-  console.log(func2());
-} catch (error) {
-  console.log({ "error": error.message }); // Output: {"error": "Not Equal"}
-}
-
-const func3 = () => ({ "value": expect(5).notToBe(null) });
-console.log(func3()); // Output: {"value": true}
