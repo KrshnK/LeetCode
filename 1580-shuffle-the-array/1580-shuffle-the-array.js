c0030589054b7d0d0e93e76c62f1889e8734a430
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-06-12 21:31:12 +0530

    Time: 61 ms (55.12%), Space: 51.2 MB (78.26%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function (nums, n) {
+    let arr = nums.splice(0,n)
+    let str = []
+    for (let i = 0; i < n; i++) {
+        str.push(arr[i])
+        str.push(nums[i])
+    }
+    return str
+};
