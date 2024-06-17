452bb61199a9d115a06f0a18a8ed52255d42fefb
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-06-17 16:49:20 +0530

    Time: 80 ms (69.14%), Space: 56.4 MB (59.31%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.js
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.js
@@ -4,22 +4,28 @@
  * @return {string[]}
  */
 var findRestaurant = function(list1, list2) {
+    // Create a map to store the indices of the elements in list1
+    let map = new Map();
+    for (let i = 0; i < list1.length; i++) {
+        map.set(list1[i], i);
+    }
+    
     let minSum = Infinity;
     let result = [];
-    
-    for (let i = 0; i < list1.length; i++) {
-        for (let j = 0; j < list2.length; j++) {
-            if (list1[i] === list2[j]) {
-                let sum = i + j;
-                if (sum < minSum) {
-                    minSum = sum;
-                    result = [list1[i]];
-                } else if (sum === minSum) {
-                    result.push(list1[i]);
-                }
+
+    // Iterate over list2 and check for common restaurants
+    for (let j = 0; j < list2.length; j++) {
+        if (map.has(list2[j])) {
+            let i = map.get(list2[j]);
+            let sum = i + j;
+            if (sum < minSum) {
+                minSum = sum;
+                result = [list2[j]];  // Reset result with the current restaurant
+            } else if (sum === minSum) {
+                result.push(list2[j]);  // Add to result if the current sum equals minSum
             }
         }
     }
-    
-    return result;
+
+    return result;  // Return the result array containing restaurants with the smallest index sum
 };
