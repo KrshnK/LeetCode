b0cf5de67876adcf58742fd7bd200c72f1e8fa79
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-06-17 16:53:33 +0530

    Time: 98 ms (28.79%), Space: 55.2 MB (81.38%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.js
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.js
@@ -4,26 +4,12 @@
  * @return {string[]}
  */
 var findRestaurant = function(list1, list2) {
-    let map = new Map();
-    for (let i = 0; i < list1.length; i++) {
-        map.set(list1[i], i);
-    }
-    
-    let minSum = Infinity;
-    let result = [];
 
-    for (let j = 0; j < list2.length; j++) {
-        if (map.has(list2[j])) {
-            let i = map.get(list2[j]);
-            let sum = i + j;
-            if (sum < minSum) {
-                minSum = sum;
-                result = [list2[j]]; 
-            } else if (sum === minSum) {
-                result.push(list2[j]);
-            }
-        }
-    }
+  const common = list1.filter(item => list2.includes(item));
 
-    return result;
+  const index1 = common.map(item => list1.indexOf(item) + list2.indexOf(item));
+
+  const min = Math.min(...index1);
+  return common.filter((item,index)=> index1[index] === min);
+   
 };
