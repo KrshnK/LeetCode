0a405bc1ac3610b4ef0306f6c132722312f841d6
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-06-18 23:12:40 +0530

    Time: 100 ms (5.34%), Space: 56.9 MB (63.47%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,5 +3,12 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-     s.reverse()
+     let first = 0
+     let last = s.length - 1
+
+     while(last > first){
+        [s[first],s[last]] = [s[last],s[first]]
+        first++
+        last--
+     }
 };
