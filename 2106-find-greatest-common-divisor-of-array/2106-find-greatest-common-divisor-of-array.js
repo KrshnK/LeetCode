7e038e9452c25074d1f13929739de4b52c0010c0
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-06-25 13:45:22 +0530

    Time: 46 ms (90.69%), Space: 49.3 MB (41.95%) - LeetHub

---
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findGCD = function(nums) {
+    let max = Math.max(...nums)
+    let min = Math.min(...nums)
+    return gcd(min,max)
+
+    function gcd(val1,val2){
+        if(val1==val2){
+            return val1
+        }
+        if(val1>val2){
+            return gcd(val1-val2,val2)
+        } else {
+            return gcd(val1,val2-val1)
+        }
+    }
+};
