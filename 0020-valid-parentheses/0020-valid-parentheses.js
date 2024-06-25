ce4a17318e2f7818f9e37d588c3bd3b1e50bccbc
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-06-25 17:47:51 +0530

    Time: 54 ms (76.43%), Space: 49.7 MB (79.42%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    let stack = [];
+    let matchingBracket = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (matchingBracket[char]) {
+            let topElement = stack.length ? stack.pop() : '#';
+            if (topElement !== matchingBracket[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
+};
