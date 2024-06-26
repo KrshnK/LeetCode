88bf294087f367e75b648ba510123a80b77421b9
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-06-26 14:43:54 +0530

    Time: 54 ms (77.84%), Space: 49 MB (88.09%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    let x = BigInt(num1)
+    let y = BigInt(num2)
+    return (x+y).toString()
+};
