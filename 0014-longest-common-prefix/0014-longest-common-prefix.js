c0417c8af866b5a76e5d66aec0f952072a25af3d
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-06-28 00:03:49 +0530

    Time: 50 ms (83.55%), Space: 49.1 MB (73.77%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let str = strs[0]
+    const len = strs.length
+    for(i=1;i<len;i++){
+       while(strs[i].indexOf(str)!==0){
+        str = str.substring(0,str.length-1)
+       }
+    }
+    return str
+};
