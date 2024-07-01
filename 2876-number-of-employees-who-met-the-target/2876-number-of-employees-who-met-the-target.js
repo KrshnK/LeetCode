66dde257eeafe24eaa8c9db462427af85f265990
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-01 16:09:03 +0530

    Time: 56 ms (81.9%), Space: 51.3 MB (16.99%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function (hours, target) {
+    let count = 0
+    const hour = hours.length
+    for (let i = 0; i < hour; i++) {
+        if (target <= hours[i]) {
+            count++
+        }
+    }
+    return count
+};
