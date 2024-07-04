c74867d59e41c7dc7897ca007c43275f43c8be97
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-04 22:56:17 +0530

    Time: 65 ms (6.54%), Space: 47.4 MB (99.78%) - LeetHub

--- 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -1,18 +1,22 @@
-function expect(val) {
-  return {
-    toBe: function (otherVal) {
-      if (val === otherVal) {
-        return true;
-      } else {
-        throw new Error("Not Equal");
-      }
-    },
-    notToBe: function (otherVal) {
-      if (val !== otherVal) {
-        return true;
-      } else {
-        throw new Error("Equal");
-      }
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe(n){
+            if(n===val) return true
+            throw 'Not Equal'
+        },
+
+        notToBe(n){
+            if(n!==val) return true
+            throw 'Equal'
+        }
     }
-  }
-}
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
