df7245c44cca0448ebe4011a1db2595f73a8d949
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-09 17:48:43 +0530

    Time: 44 ms (93.3%), Space: 49 MB (52.36%) - LeetHub

---
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function (accounts) {
+    let res = []
+    for (let i = 0; i < accounts.length; i++) {
+        let sum = 0
+        for (let j = 0; j <accounts[i].length; j++) {
+            sum += accounts[i][j]
+        }
+        res.push(sum)
+    }
+    const max = Math.max(...res)
+    return max
+};
