679883c455a8265c9ca313a9a1b2b9d232ec4f62
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-09 19:53:03 +0530

    Time: 86 ms (57.12%), Space: 63.3 MB (99.27%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.js
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.js
@@ -2,8 +2,8 @@
  * @param {number[][]} edges
  * @return {number}
  */
-var findCenter = function(edges) {
-    if(edges[0][0]==edges[1][0] || edges[0][0]==edges[1][1]){
+var findCenter = function (edges) {
+    if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
         return edges[0][0]
     }
     return edges[0][1]
