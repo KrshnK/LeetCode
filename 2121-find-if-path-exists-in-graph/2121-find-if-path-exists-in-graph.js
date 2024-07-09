9a410eddd6ae748ba7950b61f590530941a00bda
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-09 18:55:55 +0530

    Time: 788 ms (77.02%), Space: 138.7 MB (84.97%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.js
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.js
@@ -6,7 +6,14 @@
  * @return {boolean}
  */
 var validPath = function (n, edges, start, dest) {
-    const graph = createGraph(edges)
+     const graph = {}
+    for (const edge of edges) {
+        const [a, b] = edge
+        if (!(a in graph)) graph[a] = []
+        if (!(b in graph)) graph[b] = []
+        graph[a].push(b)
+        graph[b].push(a)
+    }
     return hasPath(graph, start, dest)
 };
 
