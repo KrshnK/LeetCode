2c023a39c0047e399317ac94f6abe9c5ba401619
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-09 22:15:20 +0530

    Time: 138 ms (16.54%), Space: 66.3 MB (51.77%) - LeetHub

---
+++ 2860-sort-by/2860-sort-by.js
@@ -0,0 +1,9 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function (arr, fn) {
+    let sortArr = arr.sort((a, b) => fn(a) - fn(b))
+    return sortArr
+};
