617179745cd60c38550cd72c43b8a80bb076b621
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-11 01:08:55 +0530

    Time: 90 ms (15.93%), Space: 51.3 MB (58.54%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -4,10 +4,10 @@
  */
 var missingNumber = function (nums) {
     nums.sort((a, b) => a - b)
-    const len = nums.length
+    const len = nums.length + 1
 
-    if (nums[0] !== 0) return 0
-    if (nums[len - 1] !== len) return len
+    // if (nums[0] !== 0) return 0
+    // if (nums[len - 1] !== len) return len
 
     for (let i = 0; i < len; i++) {
         if (nums[i] !== i) {
