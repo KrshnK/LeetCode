d4e27be01bf7bfe15dc519b48db0dc006c944783
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-11 01:19:26 +0530

    Time: 54 ms (80.38%), Space: 51.8 MB (47.27%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let dig = 0
+    for(const num of nums){
+        dig ^= num
+    }
+    return dig
+};
