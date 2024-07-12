bb3deb28356b4faf93058a76a032c983781c1059
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-12 17:31:45 +0530

    Time: 72 ms (22.43%), Space: 58.1 MB (11.73%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.js
+++ 0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -4,12 +4,14 @@
  */
 var minPathSum = function (grid, r = 0, c = 0, memo = {}) {
     const pos = r + ',' + c
+    const m = grid.length
+    const n = grid[0].length
 
     if (pos in memo) return memo[pos]
-    if (r >= grid.length || c >= grid[0].length) {
+    if (r >= m || c >= n) {
         return Infinity
     }
-    if (r === grid.length - 1 && c === grid[0].length - 1) {
+    if (r === m - 1 && c === n - 1) {
         return grid[r][c]
     }
 
