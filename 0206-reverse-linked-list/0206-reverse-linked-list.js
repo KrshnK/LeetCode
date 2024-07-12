581031d8f3fe662af41aab4fe7f8d09060fcf7f9
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-12 19:47:16 +0530

    Time: 55 ms (79.85%), Space: 52.9 MB (5.36%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -10,15 +10,10 @@
  * @return {ListNode}
  */
 var reverseList = function (head) {
-  let prev = null;
-
-  while (head) {
-    [head.next, prev, head] = [prev, head, head.next];
-    //   let next = head.next
-    //       head.next = prev
-    //       prev = head
-    //       head = next
-  }
-  head = prev;
-  return head;
+    let prev = null
+    while (head) {
+        [head.next, prev, head] = [prev, head, head.next]
+    }
+    head = prev
+    return head
 };
