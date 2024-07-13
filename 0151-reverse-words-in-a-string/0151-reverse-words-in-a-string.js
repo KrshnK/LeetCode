1aa39d9d73dfefe3bc4567f798a7bd8de8aaf2d5
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-13 09:14:21 +0530

    Time: 56 ms (53.8%), Space: 50.7 MB (55.03%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -2,34 +2,32 @@
  * @param {string} s
  * @return {string}
  */
-// var reverseWords = function (s) {
-//     let res = []
-//     const len = s.length
-//     let word = ""
-
-//     for (i = 0; i < len; i++) {
-//         if (s[i] !== ' ') {
-//             word += s[i]
-//         } else if (word.length > 0) {
-//             res.unshift(word)
-//             word = ""
-//         }
-//     }
-//     if (word.length > 0) {
-//         res.unshift(word)
-//     }
-
-//     return res.join(' ')
-// };
-/**
- * @param {string} s
- * @return {string}
- */
 var reverseWords = function (s) {
-    s = s.split(' ');
-    let res = [];
-    for (let i = s.length - 1; i >= 0; i--) {
-        if (s[i] != '') res.push(s[i]);
+    let res = []
+    const len = s.length
+    let word = ""
+
+    for (i = 0; i < len; i++) {
+        if (s[i] !== ' ') {
+            word += s[i]
+        } else if (word.length > 0) {
+            res.unshift(word)
+            word = ""
+        }
     }
-    return res.join(' ');
+    if (word.length > 0) {
+        res.unshift(word)
+    }
+
+    return res.join(' ')
 };
+
+
+// var reverseWords = function (s) {
+//     s = s.split(' ');
+//     let res = [];
+//     for (let i = s.length - 1; i >= 0; i--) {
+//         if (s[i] != '') res.push(s[i]);
+//     }
+//     return res.join(' ');
+// };
