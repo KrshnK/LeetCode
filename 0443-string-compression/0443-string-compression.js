c5e054e23a6fe27e5810b0be9e4d90a8ae94b94d
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-13 11:02:36 +0530

    Time: 62 ms (61.01%), Space: 52.2 MB (27.93%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.js
@@ -0,0 +1,25 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function (chars) {
+    let index = 0
+    let i = 0
+
+    while (i < chars.length) {
+        let j = i
+        while (j < chars.length && chars[i] == chars[j]) {
+            j++
+        }
+        let count = j - i
+        chars[index++] = chars[i]
+
+        if (count > 1) {
+            for (const str of count.toString()) {
+                chars[index++] = str
+            }
+        }
+        i = j
+    }
+    return index
+};
