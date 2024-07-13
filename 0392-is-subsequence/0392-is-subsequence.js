829c6263792d7a51d8060641639f730e3ced31ce
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-13 21:08:10 +0530

    Time: 57 ms (41.82%), Space: 49.2 MB (57.39%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -5,6 +5,8 @@
  */
 var isSubsequence = function (s, t) {
     let index = 0
+    if(s.length > t.length) return false
+    
         for (let j = 0 ; j < t.length; j++) {
             if (s[index] == t[j]) index++
         }
