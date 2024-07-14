33d7cd2915e145b18b97409922f590085073720c
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-14 17:30:37 +0530

    Time: 52 ms (81.68%), Space: 51.5 MB (33.19%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+    const res = []
+    const queue = [root]
+
+    if (!root) return res
+
+    while (queue.length > 0) {
+        const size = queue.length
+        const level = []
+
+        for (let i = 0; i < size; i++) {
+            const node = queue.shift();
+            level.push(node.val);
+
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+        }
+        res.push(level)
+    }
+    for (let i = 1; i < res.length; i = i + 2) {
+        res[i].reverse()
+    }
+    return res
+};
