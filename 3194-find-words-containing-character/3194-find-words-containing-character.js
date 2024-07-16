0596b6719b056603d7128e463b3d7d3c16a9ff25
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-16 22:38:03 +0530

    Time: 77 ms (50.51%), Space: 53.6 MB (14.67%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.js
+++ 3194-find-words-containing-character/3194-find-words-containing-character.js
@@ -7,11 +7,14 @@ var findWordsContaining = function(words, x) {
     let res = []
 
     for(let i=0;i<words.length;i++){
-        let set = new Set(words[i])
-        if(set.has(x)){
+        // let set = new Set(words[i])
+        // if(set.has(x)){
+        //     res.push(i)
+        // }
+        // set = []
+        if(words[i].includes(x)){
             res.push(i)
         }
-        set = []
     }
 
     return res
