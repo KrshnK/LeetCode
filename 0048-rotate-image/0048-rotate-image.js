c27c2849beb3aae0a1f862b67189d908ec23caf9
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-20 17:08:02 +0530

    Time: 48 ms (79.69%), Space: 48.8 MB (83.79%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+    let len = matrix.length
+
+    //transpose
+    for (let i = 0; i < len; i++) {
+        for (let j = i + 1; j < len; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
+        }
+    }
+
+    //reverse
+    for (let i = 0; i < len; i++) {
+        matrix[i].reverse()
+    }
+};
