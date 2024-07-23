ada440268c5e07e5f75039cbe565bcf2e5defed6
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-23 16:58:59 +0530

    Time: 92 ms (75.78%), Space: 55.1 MB (59.98%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -6,6 +6,7 @@
 var findMedianSortedArrays = function (num1, num2) {
     let merge = [...num1, ...num2].sort((a, b) => a - b)
     let median = Math.floor(merge.length / 2)
+    
     if (merge.length % 2 == 0) {
         median = (merge[median] + merge[median - 1]) / 2
     } else {
