7e3b3b07c73bca1b7a7f2ab473f3d2af9f2ce976
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-26 12:05:14 +0530

    Time: 60 ms (19.76%), Space: 49.4 MB (20.36%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.js
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.js
@@ -2,6 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(nums) {
-    return Math.min(...nums)
+var findMin = function (nums) {
+    // return Math.min(...nums)
+
+    // Another Method
+
+    let min = Infinity
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < min) {
+            min = nums[i]
+        }
+    }
+    return min
 };
