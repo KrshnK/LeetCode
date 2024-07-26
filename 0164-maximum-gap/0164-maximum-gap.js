8a7106034eb70d4819630be30d8aa6f4ff02c78a
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-26 11:01:17 +0530

    Time: 227 ms (47.99%), Space: 64.4 MB (46.15%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumGap = function (nums) {
+    if (nums.length < 2) return 0
+
+    nums.sort((a, b) => a - b)
+    let max = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i + 1] - nums[i] > max) {
+            max = nums[i + 1] - nums[i]
+        }
+    }
+
+    return max
+};
