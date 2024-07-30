e170ad5b381c5a5b963f031419451d8c7c4371ce
Krishna <113199449+KrshnK@users.noreply.github.com>
2024-07-30 14:26:00 +0530

    Time: 72 ms (27.26%), Space: 56.5 MB (5.06%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    // if(!head) return false
+    // let set = new Set()
+    // let curr = head
+
+    // while(curr){
+    //     if(set.has(curr)) return true
+    //     set.add(curr)
+    //     curr = curr.next
+    // }
+    // return false
+
+    /* Another method  */
+    if(!head) return false
+    let curr = head
+
+    while(curr && curr.next){
+        head = head.next
+        curr = curr.next.next
+        if(curr==head) return true
+    }
+
+    return false
+};
